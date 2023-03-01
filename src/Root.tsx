42615929827b3f3f9574e10569bf675d9f0b0ec6
technickal1 <31661206+technickal1@users.noreply.github.com>
2023-03-01 14:14:23 -0500

    updates

--- src/Root.tsx
+++ src/Root.tsx
@@ -16,6 +16,7 @@ export const RemotionRoot: React.FC = () => {
         defaultProps={{
           titleText: 'Welcome to the Intro',
           titleColor: 'black',
+          titlePosition: 25
         }}
       />
       <Composition
